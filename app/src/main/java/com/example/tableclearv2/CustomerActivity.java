08de4752b75d33e40bb70204778a5f1477aea15e
benjaminle9x <54958544+benjaminle9x@users.noreply.github.com>
2019-12-24 20:26:59 -0500

    Fragments

--- app/src/main/java/com/example/tableclearv2/CustomerActivity.java
+++ app/src/main/java/com/example/tableclearv2/CustomerActivity.java
@@ -1,14 +1,52 @@
 package com.example.tableclearv2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class CustomerActivity extends AppCompatActivity {
+    BottomNavigationView bottomNaviView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer);
+        findAllViews();
+
+        bottomNaviView.setOnNavigationItemSelectedListener(bottomNaviListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HomeFragment()).commit();
+
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener bottomNaviListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment selectedFragment = null;
+            switch (menuItem.getItemId()) {
+                case R.id.home:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.setting:
+                    selectedFragment = new SettingsFragment();
+                    break;
+                case R.id.assistant:
+                    selectedFragment = new AssistantFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    selectedFragment).commit();
+            return true;
+        }
+    };
+
+    private void findAllViews() {
+        bottomNaviView = findViewById(R.id.bottom_navi);
     }
 }
